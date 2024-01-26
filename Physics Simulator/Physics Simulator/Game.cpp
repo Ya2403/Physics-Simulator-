16707ac1f6bd2c2ab877ee646c36cff89eb47f66
Ya2403 <kondrachuk06@gmail.com>
2024-01-26 23:12:31 +0100

    some stupid shit added, looks like the sound domulation

--- Physics Simulator/Physics Simulator/Game.cpp
+++ Physics Simulator/Physics Simulator/Game.cpp
@@ -35,6 +35,8 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 			std::cout << "renderer creatred" << std::endl;
 		}
+
+		RandomSpawnBalls();
 		isRuning = true;
 	}
 	else
@@ -45,9 +47,16 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 void Game::update()
 {
-	Move();
-	cnt++;
-	std::cout << cnt << std::endl;
+	for (int i = 0; i < numOfBall; i++)
+	{
+		Move(i);
+	}
+
+	curBorderTime += 1 / 60.0;
+	borderX = amplitude*cos(2 * 3.14 * curBorderTime / period)+ amplitude;
+
+	if (curBorderTime > period) curBorderTime = 0;
+	//std::cout << borderX << std::endl;
 	SDL_Delay(18);
 }
 
@@ -76,8 +85,16 @@ void Game::clean()
 
 void Game::render()
 {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-	DrawCircle(renderer, x, y, 20);
+	
+
+	for (int i = 0; i < numOfBall; i++)
+	{
+		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+		DrawCircle(renderer, (int)ballsArrey[i].x, (int)ballsArrey[i].y, ballsArrey[i].radius);
+		
+	}
+	SDL_RenderDrawLineF(renderer, borderX, 0, borderX, 900);
+
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderPresent(renderer);
 	SDL_RenderClear(renderer);
@@ -121,36 +138,108 @@ void Game::DrawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, 
 	}
 }
 
-void Game::Move()
+void Game::Move(int ballIxdex)
 {
-	int radius = 20;
+	if (abs(ballsArrey[ballIxdex].vx) > 15)
+	{
+		ballsArrey[ballIxdex].vx = abs(ballsArrey[ballIxdex].vx) / ballsArrey[ballIxdex].vx * 15.0;
+	}
+
+	if (abs(ballsArrey[ballIxdex].vy) > 15)
+	{
+		ballsArrey[ballIxdex].vy = abs(ballsArrey[ballIxdex].vy) / ballsArrey[ballIxdex].vy * 15.0;
+	}
 	int windowHight;
 	int windowWidht;
 	SDL_GetWindowSize(window, &windowWidht, &windowHight);
-	x += velocityX;
-	y += velocityY;
 
-	if (x - radius < 0)
+	//float vx = (rand() % 100) / 100.0 * 20.0 - 10;
+	//float vy = (rand() % 100) / 100.0 * 20.0 - 10;
+	////std::cout << vx << std::endl;
+	//ballsArrey[ballIxdex].vy = vy;
+	ballsArrey[ballIxdex].x += ballsArrey[ballIxdex].vx;
+	ballsArrey[ballIxdex].y += ballsArrey[ballIxdex].vy;
+
+	if (ballsArrey[ballIxdex].x - ballsArrey[ballIxdex].radius < borderX)
 	{
-		x -= velocityX;
-		velocityX *= -1;
-	}else if(x + radius > windowWidht)
+		ballsArrey[ballIxdex].x = borderX + ballsArrey[ballIxdex].radius;
+		if (ballsArrey[ballIxdex].vx < 0)
+		{
+			
+			ballsArrey[ballIxdex].vx *= -1.0;
+			ballsArrey[ballIxdex].vx += abs(amplitude * (cos(2 * 3.14 * curBorderTime / period)- cos(2 * 3.14 * curBorderTime-1/60.0 / period))*30.0);
+		}
+	}
+	else if (ballsArrey[ballIxdex].x + ballsArrey[ballIxdex].radius > windowWidht)
 	{
-		x -= velocityX;
-		velocityX *= -1;
+		float x = (rand() % 100) / 100.0 * windowWidht + borderX;
+		float y = (rand() % 100) / 100.0 * windowHight;
+		float vx;
+		if ((rand() % 100) / 100.0 > 0.5)
+		{
+			vx = 10;
+		}
+		else vx = -10;
+		float vy = (rand() % 100) / 100.0 * 15.0 - 5;
+		ballsArrey[ballIxdex].x = x;
+		ballsArrey[ballIxdex].y = y;
+		ballsArrey[ballIxdex].vx = vx;
+		ballsArrey[ballIxdex].vy = vy;
 	}
 
-	if (y - radius < 0)
+	if (ballsArrey[ballIxdex].y - ballsArrey[ballIxdex].radius < 0)
 	{
-		y -= velocityY;
-		velocityY *= -1;
+		ballsArrey[ballIxdex].y -= ballsArrey[ballIxdex].vy;
+		ballsArrey[ballIxdex].vy *= -1.0;
 	}
-	else if (y + radius > windowHight)
+	else if (ballsArrey[ballIxdex].y + ballsArrey[ballIxdex].radius > windowHight)
 	{
-		y -= velocityY;
-		velocityY *= -1;
+		ballsArrey[ballIxdex].y -= ballsArrey[ballIxdex].vy;
+		ballsArrey[ballIxdex].vy *= -1.0;
 	}
-		
-		
-	
 }
+
+void Game::RandomSpawnBalls()
+{
+	int windowHight;
+	int windowWidht;
+	SDL_GetWindowSize(window, &windowWidht, &windowHight);
+	int colitionBalIndex;
+	int maxradius = 7;
+	for (int i = 0; i < NUMBEROFBALLS; i++)
+	{
+		do
+		{
+			float x = (rand() % 100) / 100.0 * windowWidht;
+			float y = (rand() % 100) / 100.0 * windowHight;
+			float vx;
+			if ((rand() % 100) / 100.0 > 0.5)
+			{
+				vx = 15;
+			}
+			else vx = -15;
+			float vy = (rand() % 100) / 100.0 * 15.0 - 5;
+
+			int radius = (rand() % 100) / 100.0 * maxradius + 2;
+			std::cout << radius << std::endl;
+			//ballsArrey[i] 
+			Ball *ball = new Ball(2, vx,vy, x, y);
+
+			ballsArrey[i] = *ball;
+			
+
+		} while (CheckForColitions(i, &colitionBalIndex));
+
+		numOfBall++;
+	}
+}
+
+bool Game::CheckForColitions(int ballIndex, int* colitionBallIndex)
+{
+	return false;
+}
+
+//void Game::lenearInterpolation(int ballIndex, int bounderyX)
+//{
+//	if(ballsArrey[ballIndex].x + ballsArrey[ballIndex].vx < bounderyX)
+//}
--- Physics Simulator/Physics Simulator/Game.h
+++ Physics Simulator/Physics Simulator/Game.h
@@ -3,6 +3,35 @@
 #include "SDL.h"
 
 #include "iostream"
+#include <cstdlib>
+
+
+struct Ball
+{
+	int radius;
+	float vx;
+	float vy;
+	float x;
+	float y;
+
+	Ball()
+	{
+		radius = 1;
+		vx = 0;
+		vy = 0;
+		x = 0;
+		y = 0;
+	}
+
+	Ball(int Radius, float Vx, float Vy, float X, float Y)
+	{
+		radius = Radius;
+		vx = Vx;
+		vy = Vy;
+		x = X;
+		y = Y;
+	}
+};
 
 class Game
 {
@@ -18,13 +47,25 @@ public:
 	void clean();
 	void DrawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_t radius);
 	bool isRuningCheck() { return isRuning; };
-	void Move();
+	void Move(int ballIxdex);
+	void RandomSpawnBalls();
+	void CalculateColition(Ball *ball1, Ball* ball2);
+	bool CheckForColitions(int ballIndex, int *colitionBallIndex);
+	void lenearInterpolation(int ballIndex, int bounderyX);
 
 
 private:
+	static const int NUMBEROFBALLS = 20000;
+	Ball ballsArrey[NUMBEROFBALLS];
+
+	float borderX = 0;
+	float period = 1;
+	float curBorderTime = 0;
+	float amplitude = 100;
+	int numOfBall = 0;
 	int x= 50;
 	int y= 90;
-	float velocityX = 2;
+	float velocityXModule = 1;
 	float velocityY = 1;
 	bool isRuning;
 	int cnt = 0;
@@ -32,5 +73,4 @@ private:
 	SDL_Renderer* renderer;
 
 };
-
 
